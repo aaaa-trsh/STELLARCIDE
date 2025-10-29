f4b23541ff2ce3e4f1304267306a7acbb316a70d
Stan-21 <shung951@gmail.com>
2025-10-28 21:18:37 -0700

    Added basic enemy that can follow players

---
+++ Assets/Scripts/EnemyScripts/PlayerAwarenessController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerAwarenessController : MonoBehaviour
+{
+    public bool AwareOfPlayer { get; private set; }
+
+    public Vector2 DirectionToPlayer {  get; private set; }
+
+    [SerializeField]
+    public float _playerAwarenessDistance;
+
+    private Transform _player;
+
+    private void Awake()
+    {
+        _player = FindFirstObjectByType<PlayerMovement>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 enemyToPlayerVector = _player.position - transform.position;
+        DirectionToPlayer = enemyToPlayerVector.normalized;
+
+        if (enemyToPlayerVector.magnitude <= _playerAwarenessDistance)
+        {
+            AwareOfPlayer = true;
+        } else
+        {
+            AwareOfPlayer = false;
+        }
+    }
+}
