7c5f186d0ded2c9a2959e6e60dfc170d3c6ad0d3
Pinkashew <87673421+Pinkashew@users.noreply.github.com>
2025-11-24 18:42:51 -0800

    menus work
    pain and suffering

---
+++ Assets/Scripts/UIScripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public UITest script;
+    public GameObject pauseMenu;
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    public static bool optionsFromGame = false;
+    void Awake()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void ReturnToMainMenu()
+    {
+        pauseMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        UITest.mainMenuActive = true;
+        optionsFromGame = false;
+    }
+
+    public void Options()
+    {
+        pauseMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        UITest.optionsMenuActive = true;
+        optionsFromGame = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Debug.Log("Esc Pressed");
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            UITest.gameActive = true;
+            UITest.pauseMenuActive = false;
+        }
+
+        if (!UITest.gameActive)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
--- Assets/Scripts/UIScripts/UITest.cs
+++ Assets/Scripts/UIScripts/UITest.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 
 public class UITest : MonoBehaviour
 {
+    public PauseMenu script;
     public GameObject mainMenu;
-    public bool mainMenuActive = true;
+    public GameObject optionsMenu;
+    public GameObject volumeMenu;
+    public GameObject pauseMenu;
+    public GameObject returnToMenu;
+    public GameObject returnToGame;
+    public static bool mainMenuActive = true;
+    public static bool optionsMenuActive = false;
+    public static bool gameActive = false;
+    public static bool pauseMenuActive = false;
     public AudioSource click;
     void Awake()
     {
@@ -12,23 +21,93 @@ public class UITest : MonoBehaviour
     }
     public void Play()
     {
-        click.Play();
-        Debug.Log("This is a test script for UI.");
+        //Debug.Log("This is a test script for UI.");
         Time.timeScale = 1f;
+        gameActive = true;
         mainMenu.SetActive(false);
+    }
+
+    public void Click()
+    {
+        click.Play();
+    }
+    public void Options()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        optionsMenuActive = true;
         mainMenuActive = false;
+    }
+
+    public void VolumeMenu()
+    {
+        optionsMenu.SetActive(false);
+        volumeMenu.SetActive(true);
+        optionsMenuActive = false;
+    }
+
+    public void Return()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        optionsMenuActive = false;
+        mainMenuActive = true;
+    }
+
+    public void ReturnFromVolume()
+    {
+        optionsMenu.SetActive(true);
+        volumeMenu.SetActive(false);
+        mainMenuActive = false;
+        optionsMenuActive = true;
+    }
+
+    public void ReturnToGame()
+    {
+        optionsMenu.SetActive(false);
+        optionsMenuActive = false;
+        mainMenuActive = false;
+        pauseMenuActive = false;
+        gameActive = true;
+        Time.timeScale = 1f;
+        //Debug.Log("Retured");
     }
 
     private void Update()
     {
-        if (mainMenuActive)
+        if (mainMenuActive || optionsMenuActive || pauseMenuActive)
         {
+            //Debug.Log("Something is Active");
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0f;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && !mainMenuActive && !optionsMenuActive)
+        {
+            //Debug.Log("Esc Pressed");
+            pauseMenu.SetActive(true);
+            pauseMenuActive = true;
+            gameActive = false;
+            //Debug.Log("Game is not active");
+        }
+        if (PauseMenu.optionsFromGame)
+        {
+            returnToMenu.SetActive(false);
+            returnToGame.SetActive(true);
         }
         else
         {
+            returnToMenu.SetActive(true);
+            returnToGame.SetActive(false);
+        }
+        if (gameActive)
+        {
+            //Debug.Log("GameActive");
+            Time.timeScale = 1f;
             Cursor.visible = false;
+            mainMenuActive = false;
+            optionsMenuActive = false;
+            pauseMenuActive = false;
             Cursor.lockState = CursorLockMode.None;
         }
     }
