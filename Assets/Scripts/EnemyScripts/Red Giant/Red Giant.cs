baf5953cfc4f3818d3be4dc659ba20429dc1c2a9
Stan-21 <shung951@gmail.com>
2026-01-23 15:39:37 -0800

    Created multi-segment enemy

---
+++ Assets/Scripts/EnemyScripts/Red Giant/Red Giant.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedGiant : MonoBehaviour
+{
+    [SerializeField] Transform head;
+    [SerializeField] Transform tail;
+    [SerializeField] int bodyLength;
+
+    public List<Segment> bodies;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
