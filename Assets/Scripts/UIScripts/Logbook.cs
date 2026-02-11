df0caa9980036ff9b186ed8b3b10c979a01ecf15
Pinkashew <realmotmgplayer@gmail.com>
2026-02-11 15:06:07 -0800

    random things

--- Assets/Scripts/UIScripts/Logbook.cs
+++ Assets/Scripts/UIScripts/Logbook.cs
@@ -1,13 +1,32 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro; // Include this if your prefab uses TextMeshPro
 
 public class LogbookManager : MonoBehaviour
 {
     public List<GameObject> tabPanels;
+    public List<string> entriesEnemies;
+    public Button buttonPrefab;
+    public Transform buttonParent;
 
     void Start()
     {
+        GenerateButtons();
         ShowTab(0);
+    }
+
+    void GenerateButtons()
+    {
+        foreach (Transform child in buttonParent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < entriesEnemies.Count; i++)
+        {
+            Button newButton = Instantiate(buttonPrefab, buttonParent);
+        }
     }
 
     public void ShowTab(int tabIndex)
---
+++ Assets/Scripts/UIScripts/LogbookEntry.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LogbookEntry : MonoBehaviour
+{
+    public Image icon;
+    public GameObject entryName;
+    public GameObject description;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
